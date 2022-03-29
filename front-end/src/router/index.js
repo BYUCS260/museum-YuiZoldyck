ece34f30c20c49d0f03969950c2f9cd3c029b101
YuiZoldyck <sunny28@byu.edu>
2022-03-28 22:25:57 -0600

    completedcompletedcompletedcompletedcompletedcompletedcompletedcompletedcompleted

--- front-end/src/router/index.js
+++ front-end/src/router/index.js
@@ -1,19 +1,19 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
-import Admin from '../views/Admin.vue'
+import HomeView from '../views/Home.vue'
+import AdminView from '../views/Admin.vue'
 
 Vue.use(VueRouter)
 
 const routes = [{
     path: '/',
     name: 'home',
-    component: Home
+    component: HomeView
   },
   {
     path: '/admin',
     name: 'admin',
-    component: Admin
+    component: AdminView
   }
 ]
 
--- front-end/vue.config.js
+++ front-end/vue.config.js
@@ -1,3 +1,4 @@
+const { defineConfig } = require('@vue/cli-service')
 module.exports = {
   devServer: {
     proxy: 'http://localhost:3000',
